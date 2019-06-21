f5f78c675c88c24ef55d55424f9e4361921ea2e9
leaimy <hantctk42@gmail.com>
2019-06-21 18:30:01 +0700

    Bo sung bai 10 ^_^

---
+++ BAI10_GAMEDOANSO/BAI10_GAMEDOANSO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI10_GAMEDOANSO
+{
+    class Program
+    {
+        static void Game()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Random rd = new Random();
+            int soCuaMay = rd.Next(501); //số ngẫu nhiên [0..500]
+            int soCuaNguoi;
+            int soLanDoan = 0;
+            Console.WriteLine("Máy đã ra 1 số [0..500], mời bạn đoán");
+            while(true)
+            {
+                soCuaNguoi = int.Parse(Console.ReadLine());
+                soLanDoan++;
+                Console.WriteLine("Số lần đoán của bạn là {0}", soLanDoan);
+                if (soCuaNguoi==soCuaMay)
+                {
+                    Console.WriteLine("Chúc mừng bạn, bạn đã đoán đúng ^_^");
+                    break;
+                }
+                if(soCuaNguoi>soCuaMay)
+                {
+                    Console.WriteLine("Số bạn đoán > Số của máy");
+                }
+                else
+                {
+                    Console.WriteLine("Số bạn đoán < Số của máy");
+                }
+                if(soLanDoan==7)
+                {
+                    Console.WriteLine("GAME OVER!@_@ bạn đã đoán quá 7 lần");
+                    Console.WriteLine("Số bạn cần đoán là {0}", soCuaMay);
+                    break;
+                }             
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Game();
+            Console.Write("Bạn có muốn chơi tiếp không (có/không):");
+            string traloi = Console.ReadLine();
+            if(traloi=="co")
+            {
+                Game();
+            }
+           
+        }
+    }
+}
