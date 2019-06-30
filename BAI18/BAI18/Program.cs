5fa600f43c887f1e5b995f4ed8788bb12d493e86
leaimy <hantctk42@gmail.com>
2019-06-30 21:49:59 +0700

    Bo sung bai huong doi tuong

---
+++ BAI18/BAI18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            SinhVien my = new SinhVien();
+            my.Ten = "Lệ Ái Mỹ";
+            my.Ma = 1115198;
+            Console.WriteLine(my);
+            string s = my.Ten;
+            SinhVien ha = new SinhVien(1812751, "Nguyễn Thị Hà");
+            Console.WriteLine(ha);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BAI18/BAI18/SinhVien.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI18
+{
+   public class SinhVien
+    {
+        #region các biến lớp
+        private int ma;
+        private string ten;
+        #endregion
+        #region các constructor
+        public SinhVien()
+        {
+            this.ma = 1;
+            this.ten = "Tên gì đó";
+        }
+        public SinhVien(int ma, string ten)
+        {
+            this.ma = ma;
+            this.ten = ten;
+        }
+        #endregion
+        #region các Properties
+        public int Ma
+        {
+            get
+            {
+                return this.ma;
+            }
+            set
+            {
+                this.ma = value;
+            }
+        }
+        public string Ten
+        {
+            set
+            {
+                this.ten = value;
+            }
+            get
+            {
+                return this.ten;
+            }
+        }
+        #endregion
+        #region các phương thức
+        public override string ToString()
+        {
+            return this.ma+"\t"+this.ten;
+        }
+        #endregion
+    }
+}
