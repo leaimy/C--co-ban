18dd882c2ea3f8b020c2035b430f8ee2290d24f4
leaimy <hantctk42@gmail.com>
2019-07-01 23:09:51 +0700

    Cap nhat bai 18 ^_^

---
+++ BAI18/BAI18/SanPham.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI18
+{
+    class SanPham
+    {
+        private int ma;
+        private string ten;
+        private int donGia;
+        public SanPham()
+        {
+            this.ma = 1;
+            this.ten = "";
+            this.donGia = 1;
+        }
+        public SanPham(int ma)
+        {
+            this.ma = ma;
+        }
+        public SanPham(int ma,string ten,int donGia)
+        {
+            this.ma = ma;
+            this.ten = ten;
+            this.donGia = donGia;
+        }
+        public void XuatHang(int x)
+        {
+
+        }
+        public void XuatHang(float X)
+        {
+
+        }
+        //Trường hợp sai:
+      /*  public string Hamx()
+        {
+            return "";
+        }
+        public float Hamx()
+        {
+            return 5;
+        }
+         */
+    }
+}
