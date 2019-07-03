c8fe4d09fe8232fc7a352dc55057e5f558e14888
leaimy <hantctk42@gmail.com>
2019-07-03 22:53:22 +0700

    Bo sung bai 19 ^_^

--- BAI19_LOPKETHUA/BAI19_LOPKETHUA/Program.cs
+++ BAI19_LOPKETHUA/BAI19_LOPKETHUA/Program.cs
@@ -38,11 +38,68 @@ namespace BAI19_LOPKETHUA
             int luong3 = mich.TinhLuong1(2);
             Console.WriteLine("Lương của Mịch=" + luong3);
         }
+        static void TestHinhHoc()
+        {
+            HinhHoc h;
+            h = new HinhHoc();
+            Console.WriteLine("Diện tích = " + h.TinhDienTich());
+
+            h = new HinhTron();
+            Console.WriteLine("Diện tích = " + h.TinhDienTich());
+        }
+        static void TestHinhChuNhatHinhVuong()
+        {
+            HinhChuNhat h1 = new HinhChuNhat(4,10);
+            Console.WriteLine("Chu vi=" + h1.ChuVi());
+            Console.WriteLine("Diện tích=" + h1.DienTich());
+
+            HinhChuNhat h2 = new HinhVuong(7);
+            Console.WriteLine("Chu vi=" + h2.ChuVi());
+            Console.WriteLine("Diện tích=" + h2.DienTich());
+
+            HinhVuong h3 = new HinhVuong(9);
+            Console.WriteLine("Chu vi=" + h3.ChuVi());
+            Console.WriteLine("Diện tích=" + h3.DienTich());
+        }
+        static void TestInterface()
+        {
+            ILamViec thi = new NhanVien();
+            Console.WriteLine(thi.LamViec("Báo cáo kinh doanh"));
+
+            ILamViec my = new SinhVien();
+            Console.WriteLine(my.LamViec("Học bài C#"));
+        }
+        static void TestHocAsVaIs()
+        {
+            List<ILamViec> ds = new List<ILamViec>();
+            NhanVien thi = new NhanVien() { Ma = 1, Ten = "Thi" };
+            ds.Add(thi);
+            SinhVien my = new SinhVien() { MaSV = "sv1", TenSV = "Mỹ" };
+            ds.Add(my);
+            ds.Add(new NhanVien() { Ma = 2, Ten = "Lệ" });
+            foreach (ILamViec i in ds)
+            {
+                if (i is NhanVien)
+                {
+                    NhanVien nv = i as NhanVien;
+                    Console.WriteLine("==>Nhân viên " + nv.Ten);
+                }
+                else if (i is SinhVien)
+                {
+                    SinhVien sv = i as SinhVien;
+                    Console.WriteLine("==>Sinh viên " + sv.TenSV);
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             TestNhanVien();
             TestNhanVien2();
+            TestHinhHoc();
+            TestHinhChuNhatHinhVuong();
+            TestInterface();
+            TestHocAsVaIs();
             Console.ReadLine();
         }
     }
