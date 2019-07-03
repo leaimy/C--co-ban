ab23e7a0d928595bb6c395fda9e9eb6574521ad0
leaimy <hantctk42@gmail.com>
2019-07-03 22:49:33 +0700

    Cạp Nhat

---
+++ BAI19_LOPKETHUA/BAI19_LOPKETHUA/HinhHoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI19_LOPKETHUA
+{
+   public class HinhHoc
+    {
+        public virtual int TinhDienTich()
+        {
+            return 0;
+        }
+    }
+}
