4078b1558655a4c3a991319615fd7e9e7165db90
leaimy <hantctk42@gmail.com>
2019-07-03 22:50:51 +0700

    Cap Nhat

---
+++ BAI19_LOPKETHUA/BAI19_LOPKETHUA/ILamViec.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI19_LOPKETHUA
+{
+    interface ILamViec
+    {
+        string LamViec(string mota);
+    }
+}
