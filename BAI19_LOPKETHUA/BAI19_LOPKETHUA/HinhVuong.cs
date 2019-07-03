c3451c75313af5ea8f54839fbfe0899983f24886
leaimy <hantctk42@gmail.com>
2019-07-03 22:47:08 +0700

    Cap Nhat

---
+++ BAI19_LOPKETHUA/BAI19_LOPKETHUA/HinhVuong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI19_LOPKETHUA
+{
+   public class HinhVuong:HinhChuNhat
+    {
+        public HinhVuong(int a):base(a,a)
+        {
+
+        }
+        public override int DienTich()
+        {
+            return base.DienTich();
+        }
+        public override int ChuVi()
+        {
+            return base.ChuVi();
+        }
+    }
+}
