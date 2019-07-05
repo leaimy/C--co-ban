65a79c661d3b1892e6728499fb53f0cf58717b65
leaimy <hantctk42@gmail.com>
2019-07-05 22:19:33 +0700

    Cap nhat bai 20 *~*

--- BAI20_QUANLINHANVIEN/BAI20_QUANLINHANVIEN/PhongBan.cs
+++ BAI20_QUANLINHANVIEN/BAI20_QUANLINHANVIEN/PhongBan.cs
@@ -33,5 +33,43 @@ namespace BAI20_QUANLINHANVIEN
                 Console.WriteLine(nv);
             }
         }
+        public NhanVien TimNhanVien(int maNv)
+        {
+            foreach (NhanVien old in dsNV)
+                if (old.MaNhanVien == maNv)
+                    return old;
+            return null;
+        }
+        public bool XoaNhanVien(int maNV)
+        {
+            NhanVien nv = TimNhanVien(maNV);
+            if (nv == null) return false;
+            dsNV.Remove(nv);
+            return true;
+        }
+        private int compare(NhanVien nv1,NhanVien nv2)
+        {
+           int kqssTen= string.Compare(nv1.TenNhanVien, nv2.TenNhanVien, true);
+            if(kqssTen==0)
+            {
+                if (nv1.MaNhanVien < nv2.MaNhanVien)
+                    return 1;
+                if (nv1.MaNhanVien > nv2.MaNhanVien)
+                    return -1;
+                return 0;
+            }
+            return kqssTen;
+        }
+        public void SapXep()
+        {
+            dsNV.Sort(compare);
+        }
+        public long  TongLuong()
+        {
+            long sum = 0;
+            foreach (NhanVien nv in dsNV)
+                sum += nv.TinhLuong;
+            return sum;
+        }
     }
 }
