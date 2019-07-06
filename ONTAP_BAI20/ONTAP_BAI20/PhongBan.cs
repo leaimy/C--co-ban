10f037f41c5955ecd38bc58dd4dc8aaecddf32c7
leaimy <hantctk42@gmail.com>
2019-07-06 23:17:59 +0700

    Bo sung bai 20 co menu

---
+++ ONTAP_BAI20/ONTAP_BAI20/PhongBan.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONTAP_BAI20
+{
+   public class PhongBan
+    {
+        private List<NhanVien> dsNV = new List<NhanVien>();
+        private string p;
+
+        public PhongBan(string p)
+        {
+            this.p = p;
+        }
+
+        public PhongBan()
+        {
+        }
+
+        public int MaPhongBan { get; set; }
+        public string TenPhongBan { get; set; }
+        public NhanVien TruongPhong { get; set; }
+        public bool ThemNhanVien(NhanVien nv)
+        {
+            bool trungMaNV = false;
+            foreach (NhanVien oldNV in dsNV)
+                if (oldNV.MaNhanVien == nv.MaNhanVien)
+                {
+                    trungMaNV = true;
+                    break;
+                }
+            if (trungMaNV == true)
+                return false;
+            dsNV.Add(nv);
+            return true;
+        }
+        public void XuatToanBoNhanVien()
+        {
+            foreach (NhanVien nv in dsNV)
+            {
+                Console.WriteLine(nv);
+            }
+        }
+        public NhanVien TimNhanVien(int maNv)
+        {
+            foreach (NhanVien old in dsNV)
+                if (old.MaNhanVien == maNv)
+                    return old;
+            return null;
+        }
+        public bool XoaNhanVien(int maNV)
+        {
+            NhanVien nv = TimNhanVien(maNV);
+            if (nv == null) return false;
+            dsNV.Remove(nv);
+            return true;
+        }
+        private int compare(NhanVien nv1, NhanVien nv2)
+        {
+            int kqssTen = string.Compare(nv1.TenNhanVien, nv2.TenNhanVien, true);
+            if (kqssTen == 0)
+            {
+                if (nv1.MaNhanVien < nv2.MaNhanVien)
+                    return 1;
+                if (nv1.MaNhanVien > nv2.MaNhanVien)
+                    return -1;
+                return 0;
+            }
+            return kqssTen;
+        }
+        public void SapXep()
+        {
+            dsNV.Sort(compare);
+        }
+        public long TongLuong()
+        {
+            long sum = 0;
+            foreach (NhanVien nv in dsNV)
+                sum += nv.TinhLuong;
+            return sum;
+        }
+    }
+}
+
---
+++ ONTAP_BAI20/ONTAP_BAI20/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONTAP_BAI20
+{
+    class Program
+    {
+        static List<PhongBan> dsPB = new List<PhongBan>();
+        
+
+        static void XuatNV()
+        {
+            PhongBan pNS = new PhongBan();
+            pNS.MaPhongBan = 1;
+            pNS.TenPhongBan = "Phòng nhân sự";
+            dsPB.Add(pNS);
+
+            NhanVien nam = new NhanVien();
+            nam.MaNhanVien = 123;
+            nam.TenNhanVien = "Nguyễn Thành Nam";
+            nam.ChucVu = LoaiChucVu.NHAN_VIEN;
+            pNS.ThemNhanVien(nam);
+
+            NhanVien nu = new NhanVien();
+            nu.MaNhanVien = 456;
+            nu.TenNhanVien = "Lê Thị Mỹ Nữ";
+            nu.ChucVu = LoaiChucVu.TRUONG_PHONG;
+            pNS.ThemNhanVien(nu);
+
+            NhanVien n = new NhanVien();
+            n.MaNhanVien = 357;
+            n.TenNhanVien = "Nguyễn Thành Nam";
+            n.ChucVu = LoaiChucVu.NHAN_VIEN;
+            pNS.ThemNhanVien(n);
+
+            PhongBan pkt = new PhongBan();
+            pkt.MaPhongBan = 2;
+            pkt.TenPhongBan = "Phòng kế toán";
+            dsPB.Add(pkt);
+            NhanVien hoa = new NhanVien();
+            hoa.MaNhanVien = 789;
+            hoa.TenNhanVien = "Nguyễn Hoa";
+            hoa.ChucVu = LoaiChucVu.PHO_PHONG;
+            pkt.ThemNhanVien(hoa);
+            NhanVien thao = new NhanVien();
+            thao.MaNhanVien = 555;
+            thao.TenNhanVien = "Châu lệ Thảo";
+            thao.ChucVu = LoaiChucVu.NHAN_VIEN;
+            pkt.ThemNhanVien(thao);
+
+            PhongBan ppt = new PhongBan();
+            ppt.MaPhongBan = 3;
+            ppt.TenPhongBan = "Phòng phát triển";
+            dsPB.Add(ppt);
+            NhanVien quoc = new NhanVien();
+            quoc.MaNhanVien = 246;
+            quoc.TenNhanVien = "Lê Minh Quốc";
+            quoc.ChucVu = LoaiChucVu.NHAN_VIEN;
+            ppt.ThemNhanVien(quoc);
+            NhanVien anh = new NhanVien();
+            anh.MaNhanVien = 876;
+            anh.TenNhanVien = "Bùi Tú Anh";
+            anh.ChucVu = LoaiChucVu.TRUONG_PHONG;
+            ppt.ThemNhanVien(anh);
+
+            Console.WriteLine("Danh sách toàn bộ nhân viên trong công ty:");
+
+            foreach (PhongBan pb in dsPB)
+            {
+                Console.WriteLine(pb.TenPhongBan);
+                pb.XuatToanBoNhanVien();
+            }
+        }
+        static void SuaNV()
+        {
+            PhongBan pNS = new PhongBan();
+            pNS.MaPhongBan = 1;
+            pNS.TenPhongBan = "Phòng nhân sự";
+            dsPB.Add(pNS);
+
+            NhanVien nam = new NhanVien();
+            nam.MaNhanVien = 123;
+            nam.TenNhanVien = "Nguyễn Thành Nam";
+            nam.ChucVu = LoaiChucVu.NHAN_VIEN;
+            pNS.ThemNhanVien(nam);
+
+            NhanVien nu = new NhanVien();
+            nu.MaNhanVien = 456;
+            nu.TenNhanVien = "Lê Thị Mỹ Nữ";
+            nu.ChucVu = LoaiChucVu.TRUONG_PHONG;
+            pNS.ThemNhanVien(nu);
+
+            NhanVien n = new NhanVien();
+            n.MaNhanVien = 357;
+            n.TenNhanVien = "Nguyễn Thành Nam";
+            n.ChucVu = LoaiChucVu.NHAN_VIEN;
+            pNS.ThemNhanVien(n);
+
+            PhongBan pkt = new PhongBan();
+            pkt.MaPhongBan = 2;
+            pkt.TenPhongBan = "Phòng kế toán";
+            dsPB.Add(pkt);
+            NhanVien hoa = new NhanVien();
+            hoa.MaNhanVien = 789;
+            hoa.TenNhanVien = "Nguyễn Hoa";
+            hoa.ChucVu = LoaiChucVu.PHO_PHONG;
+            pkt.ThemNhanVien(hoa);
+            NhanVien thao = new NhanVien();
+            thao.MaNhanVien = 555;
+            thao.TenNhanVien = "Châu lệ Thảo";
+            thao.ChucVu = LoaiChucVu.NHAN_VIEN;
+            pkt.ThemNhanVien(thao);
+
+            PhongBan ppt = new PhongBan();
+            ppt.MaPhongBan = 3;
+            ppt.TenPhongBan = "Phòng phát triển";
+            dsPB.Add(ppt);
+            NhanVien quoc = new NhanVien();
+            quoc.MaNhanVien = 246;
+            quoc.TenNhanVien = "Lê Minh Quốc";
+            quoc.ChucVu = LoaiChucVu.NHAN_VIEN;
+            ppt.ThemNhanVien(quoc);
+            NhanVien anh = new NhanVien();
+            anh.MaNhanVien = 876;
+            anh.TenNhanVien = "Bùi Tú Anh";
+            anh.ChucVu = LoaiChucVu.TRUONG_PHONG;
+            ppt.ThemNhanVien(anh);
+
+            Console.WriteLine("Danh sách toàn bộ nhân viên trong công ty:");
+
+            foreach (PhongBan pb in dsPB)
+            {
+                Console.WriteLine(pb.TenPhongBan);
+                pb.XuatToanBoNhanVien();
+            }
+           
+            NhanVien old =pNS.TimNhanVien(123);
+            if (old != null)
+            {
+
+                old.TenNhanVien = "Nguyễn Minh Tuệ";
+            }
+
+            Console.WriteLine("Danh sách toàn bộ nhân viên trong công ty sau khi sửa:");
+
+            foreach (PhongBan pb in dsPB)
+            {
+                Console.WriteLine(pb.TenPhongBan);
+                pb.XuatToanBoNhanVien();
+            }
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
+            {
+                Menu();
+                Console.WriteLine("Bạn có muốn tiếp tục (c/k):");
+                string traloi = Console.ReadLine();
+                if (traloi == "k")
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("===========HỆ THỐNG CHỨC NĂNG===========");
+            Console.WriteLine("1. Xuất nhân viên");
+            Console.WriteLine("2. Sửa nhân viên");
+            Console.WriteLine("3. Xóa nhân viên");
+            Console.WriteLine("4. Sắp xếp nhân viên");
+            Console.WriteLine("5. Tìm nhân viên");
+            Console.WriteLine("6. Tổng số lương phải trả trong 1 tháng");
+            Console.WriteLine("Chọn chức năng [1..6] là:");
+            int chonMenu = int.Parse(Console.ReadLine());
+            switch(chonMenu)
+            {
+                case 1:
+                    Console.WriteLine("1. Xuất nhân viên");
+                    XuatNV();
+                    break;
+                case 2:
+                    Console.WriteLine("2. Sửa nhân viên");
+                    SuaNV();
+                    break;
+                case 3:
+                    Console.WriteLine("3. Xóa nhân viên");
+                    break;
+                case 4:
+                    Console.WriteLine("4. Sắp xếp nhân viên");
+                    break;
+                case 5:
+                    Console.WriteLine("5. Tìm nhân viên");
+                    break;
+                case 6:
+                    Console.WriteLine("6. Tổng số lương phải trả trong 1 tháng");
+                    break;
+            }
+        }
+    }
+}
